3ce7a43ff9cc8271c86f4f4fe1f4bae086858af1
NamanCodes <95081384+Naman-sCode@users.noreply.github.com>
2023-12-26 18:30:37 +0530

    Add files via upload
    Simple button app with an AppBar while learning APP Development

--- lib/main.dart
+++ lib/main.dart
@@ -1,170 +1,157 @@
 import 'package:flutter/material.dart';
 
+// import 'package:flutter/material.dart';
+
 void main() => runApp(const MaterialApp(
-      home: IDCard(),
+      home: Home(),
     ));
-// void main() {
-//   runApp(const MaterialApp());
-// }
 
-class IDCard extends StatelessWidget {
-  const IDCard({super.key});
-
-  // const id_card({super.key});
-  // const home({super.key});
+class Home extends StatelessWidget {
+  const Home();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('ID Card', style: TextStyle(color: Colors.white)),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        elevation: 0.0,
+          title: const Text('APP'),
+          centerTitle: true,
+          backgroundColor: Colors.green[200]),
+      body: Row(
+        //Column
+        // mainAxisAlignment: MainAxisAlignment.center,
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        // children: <Widget>[
+          // Expanded(child: Image.asset('assets/Image-2.png')),
+          // Expanded(
+          //   flex: 1,
+          //   child: Container(
+          //       padding: const EdgeInsets.all(30),
+          //       color: Colors.blue,
+          //       child: const Text('1')),
+          // ),
+          // Expanded(
+          //   flex: 2,
+          //   child: Container(
+          //       padding: const EdgeInsets.all(30),
+          //       color: Colors.red,
+          //       child: const Text('2')),
+          // ),
+          // Expanded(
+          //   flex: 3,
+          //   child: Container(
+          //       padding: const EdgeInsets.all(30),
+          //       color: Colors.yellow,
+          //       child: const Text('3')),
+          // )
+        // ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-              child: CircleAvatar(
-                radius: 40,
-                backgroundImage: AssetImage('assets/Image-3.jpeg'),
-              ),
-            ),
-//               child: CircleAvatar(
-//                 radius: 40.0,
-//                 backgroundImage: AssetImage('assets/Image-3.jpeg'),
-//                 child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.center,
-//                 children: [
-//                   CircleAvatar(
-//                     radius: 40.0,
-//                     backgroundImage: AssetImage('assets/Image-3.jpeg'),
-//                   ),
-//                   SizedBox(height: 8.0), // Adjust the height based on your design
-//                   Text(
-//                     'VIT BHOPAL',
-//                     style: TextStyle(
-//                       // Add any text style properties as needed
-//                       color: Colors.black,
-//                       fontSize: 10.0,
-//                     ),
-//                   ),
-//     ],
-//   ),
-// ),
-//             ),
 
-            //       mainAxisAlignment: MainAxisAlignment.center,
-            //       children: [
-            //         SizedBox(height: 60.0), // Adjust the height based on your design
-            //         Text(
-            //           'VIT BHOPAL',
-            //           style: TextStyle(
-            //             // Add any text style properties as needed
-            //             color: Colors.black,
-            //             fontSize: 12.0,
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //   ),
-            // ),
+      // Container(
+      //   padding: const EdgeInsets.all(30),
+      //   color: Colors.pink,
+      //   child: const Text('1'),
+      // ),
+      // Container(
+      //   padding: const EdgeInsets.all(40),
+      //   color: Colors.cyan,
+      //   child: const Text('2'),
+      // ),
+      // Container(
+      //   padding: const EdgeInsets.all(50),
+      //   color: Colors.amber[300],
+      //   child: const Text('3'),
+      // ),
+      //   ],
+      // ),
+      // Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: <Widget>[
+      //     const Text('Hey'),
+      //     ElevatedButton(
+      //       onPressed: () {},
+      //       // label: Text('ns'),
+      //       // color: Colors.limeAccent,
+      //       child: const Text('Hey'),
+      //     ),
+      //     Container(
+      //     color: Colors.cyan,
+      //     padding: const EdgeInsets.all(30.0),
+      //     child: const Text('inside container')
+      //   ),
+      //   ],
+      // ),
 
-            // Center(
-            //   child: CircleAvatar(
-            //     radius: 40.0,
-            //     backgroundImage: AssetImage('assets/Image-3.jpeg'),
-            //     child: Text('VIT BHOPAL'),
-            //   ),
-            //   // child: Text('')
-            // ),
-            Divider(
-              color: Colors.blue,
-              height: 60.0,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.grey,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'Naman',
-              style: TextStyle(
-                color: Colors.black,
-                // color: Color.fromARGB(255, 255, 215, 64),
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'HOMETOWN',
-              style: TextStyle(
-                color: Colors.grey,
-                // color: Color.fromARGB(255, 238, 238, 238),
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              'JBP, MP',
-              style: TextStyle(
-                color: Colors.black,
-                // color: Color.fromARGB(255, 255, 215, 64),
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text(
-              'Registration Number',
-              style: TextStyle(
-                color: Colors.grey,
-                // color: Color.fromARGB(255, 238, 238, 238),
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 10.0),
-            Text(
-              '21BCE*0*7*',
-              style: TextStyle(
-                color: Colors.black,
-                // color: Color.fromARGB(255, 238, 238, 238),
-                fontWeight: FontWeight.bold,
-                fontSize: 28.0,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Color.fromARGB(255, 189, 189, 189),
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'namankumar@mail.com',
-                  style: TextStyle(
-                    color: Colors.black,
-                    // color: Color.fromARGB(255, 189, 189, 189),
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+      // PADDDING WIDGET.
+
+      // const Padding(
+      //   padding: EdgeInsets.all(30),
+      //   child: Text('hello'),
+      // ),
+
+      // THE CONTAINER WIDGET.
+
+      // Container(
+      //   padding: const EdgeInsets.all(20),
+      //   margin: const EdgeInsets.all(30),
+      //   color: Colors.grey[300],
+      //   child: const Text('Hello'),
+      // ),
+
+      // Center(
+      //   child: ElevatedButton.icon(
+      //     onPressed: (){},
+      //     icon: const Icon(
+      //       Icons.mail,
+      //     ), label: const Text('Mail'),
+      //   )
+
+      // NOT WORKING
+
+      // FlatButton(
+      //   onPressed:(){},
+      //   child: null
+      // ),
+
+      // ELEVATED BUTTON ON THE CENTER OF THE SCREEN.
+
+      // ElevatedButton(
+      //   onPressed: () {
+      //Empty function button does nothing when presssed.
+      //      print('Done Something');
+      // },
+      // child: const Text('Do something'),
+
+      // ICONS
+
+      // Icon(
+      //   Icons.airport_shuttle,
+      //   color: Colors.lightBlue,
+      //   size: 50
+
+      // STYLED TEXT.
+
+      // Text(
+      //   'Hey Mayur!. This is the app.',
+      //   style: TextStyle(
+      //     fontSize: 20.0,
+      //     fontWeight: FontWeight.bold,
+      //     letterSpacing: 2.0,
+      //     // color: Colors.grey[600],
+      //     fontFamily: 'IndieFlower',
+      //   ),
+      // ),
+
+      // IMAGE INSERTION ON THE PAGE, USING LINK AND LOCAL ASSETS USING PUBSPEC.YAML
+
+      // Image(image: NetworkImage('https://images.unsplash.com/photo-1702145754106-05d909f08c9d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'))
+      // Image(image: AssetImage('assets/Image-1.png'),),
+      // ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green[200],
+        // child: const Icon(Icons.add),
+        child: const Text('Click'),
       ),
     );
   }
